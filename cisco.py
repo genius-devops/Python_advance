4503970e9c7af1e924e2e870f5cafc5e51895308
genius-devops <adamslabaran@gmail.com>
2023-10-01 11:56:08 +0100

    end of literals

--- cisco.py
+++ cisco.py
@@ -120,5 +120,48 @@ print(2 * 3 % 5)   # 1
 print((2 ** 4), (2 * 4.), (2 * 4))  # 16 8.0 8
 print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))   # -0.5 0.5 0 -1
 print((2 % -4), (2 % 4), (2 ** 3 ** 2))  # -2 2 512
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5  # same as  c = math.sqrt(a ** 2 + b ** 2)
+print("c =", c) # c = 5.0
+
+john = 3
+mary = 5
+adam = 6
+
+print(john, mary, adam, sep=',')
+
+total_apples = john + mary + adam
+print(total_apples)
+
+# peter = 12.5
+# suzy = 2
+# print(peter / suzy)
+# print("Total number of apples:", total_apples)
+
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+
+x = 0
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = 1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = -1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
 
 
